8fcad79d96ebc3e8d8a169d757b615c9a7fe4cf1
Arkail <83088623+Arkail@users.noreply.github.com>
2021-05-26 21:45:42 -0300

    cambie el codigo de carpeta
    hice lo del titulo

---
+++ src/Años.java
@@ -0,0 +1,31 @@
+public class Años{
+  public static void main(String [] args){
+      String dia = "viernes";
+      String año = "2021";
+      String barra ="/";
+      for(int m = 1; m <= 12; m++){
+              if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+
+                  for (int d1 = 1; d1 <= 31; d1++) {
+                      System.out.println(d1 + barra + m + barra + año);
+                  }
+
+              }
+              if (m == 2) {
+                  for (int d2 = 1; d2 < 29; d2++) {
+
+                      System.out.println(d2 + barra + m + barra + año);
+                  }
+              }
+              if (m == 4 || m == 6 || m == 9 || m == 11) {
+                  for (int d3 = 1; d3 <= 30; d3++) {
+                      System.out.println(d3 + barra + m + barra + año);
+                  }
+
+
+              }
+
+     }
+
+  }
+}
