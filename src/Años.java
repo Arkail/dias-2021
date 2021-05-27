a7e255f143757a430cd2237777f043085f0a7fe2
Arkail <83088623+Arkail@users.noreply.github.com>
2021-05-26 22:34:31 -0300

    agregue los dias

--- src/Años.java
+++ src/Años.java
@@ -1,25 +1,70 @@
 public class Años{
   public static void main(String [] args){
-      String dia = "viernes";
-      String año = "2021";
-      String barra ="/";
+      String dia = "jueves";
       for(int m = 1; m <= 12; m++){
               if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
 
                   for (int d1 = 1; d1 <= 31; d1++) {
-                      System.out.println(d1 + barra + m + barra + año);
+
+                      if(dia.equals("domingo")) {
+                             dia = "lunes";
+                      }else if(dia.equals("lunes")){
+                          dia = "martes";
+                      }else if(dia.equals("martes")){
+                          dia = "miercoles";
+                      }else if(dia.equals("miercoles")){
+                          dia = "jueves";
+                      }else if(dia.equals("jueves")){
+                          dia = "viernes";
+                      }else if(dia.equals("viernes")){
+                          dia = "sabado";
+                      }else if(dia.equals("sabado")){
+                          dia = "domingo";
+                      }
+                      System.out.println(dia + " " + d1 + " de " + m + " de 2021");
                   }
 
               }
               if (m == 2) {
                   for (int d2 = 1; d2 < 29; d2++) {
+                      if(dia.equals("domingo")) {
+                          dia = "lunes";
+                      }else if(dia.equals("lunes")){
+                          dia = "martes";
+                      }else if(dia.equals("martes")){
+                          dia = "miercoles";
+                      }else if(dia.equals("miercoles")){
+                          dia = "jueves";
+                      }else if(dia.equals("jueves")){
+                          dia = "viernes";
+                      }else if(dia.equals("viernes")){
+                          dia = "sabado";
+                      }else if(dia.equals("sabado")){
+                          dia = "domingo";
+                      }
 
-                      System.out.println(d2 + barra + m + barra + año);
+
+                      System.out.println(dia + " " + d2 + " de " + m + " de 2021");
                   }
               }
               if (m == 4 || m == 6 || m == 9 || m == 11) {
                   for (int d3 = 1; d3 <= 30; d3++) {
-                      System.out.println(d3 + barra + m + barra + año);
+                      if(dia.equals("domingo")) {
+                          dia = "lunes";
+                      }else if(dia.equals("lunes")){
+                          dia = "martes";
+                      }else if(dia.equals("martes")){
+                          dia = "miercoles";
+                      }else if(dia.equals("miercoles")){
+                          dia = "jueves";
+                      }else if(dia.equals("jueves")){
+                          dia = "viernes";
+                      }else if(dia.equals("viernes")){
+                          dia = "sabado";
+                      }else if(dia.equals("sabado")){
+                          dia = "domingo";
+                      }
+                      System.out.println(dia + " " + d3 + " de " + m + " de 2021");
                   }
 
 
